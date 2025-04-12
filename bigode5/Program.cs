0d5851edd7c85763feecc7c292b403f9df8f7efa
LordBigode44 <el.pedroso.de.oliveira@gmail.com>
2025-04-11 21:22:20 -0300

    Add files via upload

---
+++ bigode5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class program{
+    static void Main(){
+        
+        int number1;
+        Console.WriteLine("insira um numero qualquer: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+       
+        
+        if(number1 %2 == 0){
+            Console.WriteLine("o numero é par");
+        }else{
+            Console.WriteLine ("o numero é impar");
+        }
+    }
+}
