b85472791f2ed3545951a83b081365e4d496a1d7
LordBigode44 <el.pedroso.de.oliveira@gmail.com>
2025-04-11 21:20:59 -0300

    Add files via upload

---
+++ bigode3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        double number1, number2, number3;
+
+        Console.WriteLine("Insira a nota 1: ");
+        number1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira a nota 2: ");
+        number2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira a nota 3: ");
+        number3 = double.Parse(Console.ReadLine());
+
+        double answer = ((number1 + number2 + number3) / 3);
+        Console.WriteLine("Resultado da média: " + answer);
+    }
+}
