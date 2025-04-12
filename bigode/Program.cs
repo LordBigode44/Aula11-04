94d6af94b6e6de888aba030e123a6c917d6a907e
LordBigode44 <el.pedroso.de.oliveira@gmail.com>
2025-04-11 21:20:15 -0300

    Add files via upload

---
+++ bigode/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        String name;
+        Console.Write("White your name: ");
+        name =  Console.ReadLine();
+        Console.WriteLine($"hello, {name}! wellcome");
+    }
+}
---
+++ bigode2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class program{
+    static void Main(){
+        
+        
+        int number1;
+        Console.Write("insira seu ano de nacimento ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+        
+        int answer = 2024 - number1;
+        Console.WriteLine("resultado da soma: "+answer);
+    }
+}
