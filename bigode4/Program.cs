490756ca131f91fed26861b3e83eaec97633cd4a
LordBigode44 <el.pedroso.de.oliveira@gmail.com>
2025-04-11 21:21:32 -0300

    Add files via upload

---
+++ bigode4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class program{
+    static void Main(){
+        
+        int number1, number2;
+        Console.WriteLine("insira o numero 1: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("insira o numero 2: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+        
+        if(number1 > number2){
+            Console.WriteLine($"numero maior é " + number1);
+        }else{
+            Console.WriteLine($"numero maior é " + number2);
+        }
+    }
+}
